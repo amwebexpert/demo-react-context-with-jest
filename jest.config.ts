e9fd0dce40ae73da9416b87231eaa0a2bdc93996
Andre-3rd-Viking <andre@t-b.ca>
2024-02-11 12:32:38 -0600

    chore: test structure

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ export default {
     // process `*.tsx` files with `ts-jest`
   },
   moduleNameMapper: {
-    "\\.(css|less|sass|scss)$": "<rootDir>/src/test/__mocks__/styleMock.js",
-    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/src/test/__ mocks __/fileMock.js",
+    "\\.(css|less|sass|scss)$": "<rootDir>/test/__mocks__/styleMock.js",
+    "\\.(gif|ttf|eot|svg|png)$": "<rootDir>/test/__ mocks __/fileMock.js",
   },
 };
--- test/App.test.tsx
+++ test/App.test.tsx
@@ -1,7 +1,7 @@
 import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 
-import App from "../App";
+import App from "../src/App";
 
 describe("App tests suite", () => {
   it("should display", () => {
